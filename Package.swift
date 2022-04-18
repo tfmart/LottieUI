e80e314c815b4ff730cbf4869b6f9b80843e52b5
Tomás Martins <tomas.martins@guru.com.vc>
2022-04-18 18:14:04 -0300

    First commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "LottieUI",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,15 +15,14 @@ let package = Package(
             targets: ["LottieUI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Lottie", url: "https://github.com/airbnb/lottie-ios.git", from: "3.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LottieUI",
-            dependencies: []),
+            dependencies: ["Lottie"]),
         .testTarget(
             name: "LottieUITests",
             dependencies: ["LottieUI"]),
--- Sources/LottieUI/LottieUI.swift
+++ Sources/LottieUI/LottieUI.swift
@@ -1,6 +1,91 @@
-public struct LottieUI {
-    public private(set) var text = "Hello, World!"
+//
+//  Lottie.swift
+//  GuruDesignSystem
+//
+//  Created by Tomas Martins on 18/04/22.
+//  Copyright © 2022 Tomas Martins. All rights reserved.
+//
 
-    public init() {
+import Lottie
+import SwiftUI
+
+@available(iOS 13.0, *)
+private struct Lottie: UIViewRepresentable {
+    public typealias UIViewType = AnimationView
+    // Initializer properties
+    internal var name: String
+    internal var bundle: Bundle = .main
+    internal var imageProvider: AnimationImageProvider?
+    internal var animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache
+    
+    // Modifier properties
+    @State internal var loopMode: LottieLoopMode = .playOnce
+    @State internal var isPlaying: Bool = true
+    @State internal var frame: AnimationFrameTime = 0
+    @State internal var initialFrame: AnimationFrameTime?
+    @State internal var finalFrame: AnimationFrameTime?
+
+    public init(
+        _ name: String,
+        bundle: Bundle = .main,
+        imageProvider: AnimationImageProvider? = nil,
+        animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache
+    ) {
+        self.name = name
+        self.bundle = bundle
+        self.imageProvider = imageProvider
+        self.animationCache = animationCache
+    }
+
+    func makeUIView(context: Context) -> AnimationView {
+        let animation = Animation.named(name, bundle: bundle, subdirectory: nil, animationCache: animationCache)
+        let provider = imageProvider ?? BundleImageProvider(bundle: bundle, searchPath: nil)
+        let animationView = AnimationView(animation: animation, imageProvider: provider)
+        animationView.loopMode = loopMode
+        return animationView
+    }
+
+    func updateUIView(_ uiView: AnimationView, context: Context) {
+        uiView.loopMode = self.loopMode
+        self.frame = uiView.currentFrame
+        if isPlaying {
+            if let initialFrame = initialFrame,
+               let finalFrame = finalFrame {
+                uiView.play(fromFrame: initialFrame, toFrame: finalFrame, loopMode: loopMode) { completed in
+                    isPlaying = !completed
+                }
+            } else {
+                uiView.play { completed in
+                    isPlaying = !completed
+                }
+            }
+        } else {
+            uiView.stop()
+            isPlaying = false
+        }
     }
 }
+
+extension View where Self == Lottie {
+    func loopMode(_ mode: LottieLoopMode) -> some View {
+        self.loopMode = mode
+        return self
+    }
+
+    func play(_ isPlaying: Bool) -> some View {
+        self.isPlaying = isPlaying
+        return self
+    }
+
+    func onFrame(_ completion: (AnimationFrameTime) -> Void) -> some View {
+        completion(self.frame)
+        return self
+    }
+
+    func play(fromFrame initialFrame: AnimationFrameTime?, to finalFrame: AnimationFrameTime?) -> some View {
+        self.initialFrame = initialFrame
+        self.finalFrame = finalFrame
+        return self
+    }
+}
+
