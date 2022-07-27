56358303c2c375a94f7d5e7cb577e700fd51fe12
Tomás Martins <tomas.marti@me.com>
2022-07-26 22:32:13 -0300

    Fixed async preview

--- Sources/LottieUI/AsyncLottieView/AsyncLottieView.swift
+++ Sources/LottieUI/AsyncLottieView/AsyncLottieView.swift
@@ -70,11 +70,23 @@ public struct AsyncLottieView<Content: View>: View {
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        AsyncLottieView(url: .init(string: "https://assets3.lottiefiles.com/packages/lf20_hbdelex6.json")!) { lottieView in
-            lottieView
-                .loopMode(.loop)
-        } placeholder: {
-            Text("Loading")
+        AsyncLottieView(url: .init(string: "https://assets3.lottiefiles.com/packages/lf20_hbdelex6.json")!) { phase in
+            switch phase {
+            case .loading:
+                Text("Loading...")
+            case .error:
+                Text("Error")
+            case .success(let lottieView):
+                lottieView
+                    .loopMode(.loop)
+                    .renderingEngine(.coreAnimation)
+            }
         }
+//        AsyncLottieView(url: .init(string: "https://assets3.lottiefiles.com/packages/lf20_hbdelex6.json")!) { lottieView in
+//            lottieView
+//                .loopMode(.loop)
+//        } placeholder: {
+//            Text("Loading")
+//        }
     }
 }
