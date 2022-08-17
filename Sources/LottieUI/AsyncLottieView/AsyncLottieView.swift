57b0a7d16649ef9a40e798fcfef773298944bde4
Tomás Martins <tomas.marti@me.com>
2022-08-17 20:48:49 -0300

    Removed init with private api

--- Sources/LottieUI/AsyncLottieView/AsyncLottieView.swift
+++ Sources/LottieUI/AsyncLottieView/AsyncLottieView.swift
@@ -16,29 +16,6 @@ public struct AsyncLottieView<Content: View>: View {
 
     @State private var phase: AsyncLottiePhase = .loading
     
-    /// Creates a view that presents an animation from a remote URL
-    /// - Parameters:
-    ///   - url: The URL of the Lottie animation to be displayed
-    ///   - animationCache: Cache to improve performance when playing recurrent animations.
-    ///   - animation: A closure that takes a ``LottieView`` as an input and returns a view with the animation to be displayed. You can modify the animation view as needed before running it.
-    ///   - placeholder: A closure that returns the view to be displayed until the Lottie is successfully downloaded.
-    // TODO: - Remove private API code (_ConditionalContent)
-//    public init<L, P>(
-//        url: URL,
-//        animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache,
-//        @ViewBuilder animation: @escaping (LottieView) -> L,
-//        @ViewBuilder placeholder: @escaping () -> P
-//    ) where Content == _ConditionalContent<L, P>, L: View, P: View {
-//        self.url = url
-//        self.cacheProvider = animationCache
-//        self.content = { phase -> _ConditionalContent<L, P> in
-//            switch phase {
-//            case .success(let lottieView): return ViewBuilder.buildEither(first: animation(lottieView))
-//            default: return ViewBuilder.buildEither(second: placeholder())
-//            }
-//        }
-//    }
-    
     /// Creates a view that presents a Lottie animation from a remote URL to be displayed in phases
     /// - Parameters:
     ///   - url: The URL of the Lottie animation to be displayed
